1190bd376fcc0950a872ce20a67681cb5cbc15a4
jinweile <jinweile@163.com>
2015-04-19 04:29:35 +0800

    update

--- sqlserver.pl
+++ sqlserver.pl
@@ -6,9 +6,9 @@ my $database="tgz";
 my $user="sa";
 my $auth="123456";
   
-BEGIN{
-	$ENV{SYBASE} = "/usr/local";
-}
+# BEGIN{
+# 	$ENV{SYBASE} = "/usr/local";
+# }
   
 # Connect to the SQL Server Database
 my $dbh = DBI->connect("dbi:Sybase:server=SS_MY_DB;database=$database",
@@ -16,7 +16,7 @@ my $dbh = DBI->connect("dbi:Sybase:server=SS_MY_DB;database=$database",
 	$auth,
 	{RaiseError => 1, AutoCommit => 1}
 ) || die "Database connection not made: $DBI::errstr";
-$sth = $dbh->prepare("select * from [User]");#准备
+$sth = $dbh->prepare("select top 10 * from [Customers]");#准备
 $sth->execute();#执行
 while(@ary = $sth->fetchrow_array()){
 	print join("\t",@ary),"\n";#打印抽取结果
