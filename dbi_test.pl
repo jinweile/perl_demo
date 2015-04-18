675dd1713a96c8f8f8dce1fe2342f6d5c0c9dd59
jinweile <jinweile@163.com>
2015-04-19 03:07:29 +0800

    update

--- dbi_test.pl
+++ dbi_test.pl
@@ -7,7 +7,7 @@ my $user = 'root';
 my $password = '123456';
 my ($dbh,$sth,@ary);
 $dbh = DBI->connect($dsn,$user,$password);#连接数据库
-$sth = $dbh->prepare("select * from ACCESSTOKEN");   #准备
+$sth = $dbh->prepare("select * from ACCESSTOKEN");#准备
 $sth->execute();#执行
 while(@ary = $sth->fetchrow_array()){
 	print join("\t",@ary),"\n";#打印抽取结果
