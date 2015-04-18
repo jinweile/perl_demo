37146c65a2e0476eb76921d2e20ba742dd33050f
jinweile <jinweile@163.com>
2015-04-19 04:25:38 +0800

    加入sqlserver的demo

---
+++ sqlserver.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+use DBI;
+use DBD::Sybase;
+  
+my $database="tgz";
+my $user="sa";
+my $auth="123456";
+  
+BEGIN{
+	$ENV{SYBASE} = "/usr/local";
+}
+  
+# Connect to the SQL Server Database
+my $dbh = DBI->connect("dbi:Sybase:server=SS_MY_DB;database=$database",
+	$user,
+	$auth,
+	{RaiseError => 1, AutoCommit => 1}
+) || die "Database connection not made: $DBI::errstr";
+$sth = $dbh->prepare("select * from [User]");#准备
+$sth->execute();#执行
+while(@ary = $sth->fetchrow_array()){
+	print join("\t",@ary),"\n";#打印抽取结果
+}
+$sth->finish;#结束句柄
+$dbh->disconnect;#断开
