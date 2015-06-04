725336c5498c0fae4595922820ab70ddc1831f87
jinweile <jinweile@163.com>
2015-06-04 23:11:35 +0800

    fix bug

--- hash_sort.pl
+++ hash_sort.pl
@@ -3,7 +3,7 @@
 use strict;
 use Digest::MD5 qw(md5_hex);
  
-my $request = "manual=true&model=iPhone%20Simulator&ver=2.7.2&signKey=43087115F6EDFC293E14C9C036C3F885";
+my $request = "manual=&model=iPhone%20Simulator&ver=2.7.2&signKey=43087115F6EDFC293E14C9C036C3F885";
 my $md5result = md5_hex($request);
 print $md5result."\n";
 
@@ -11,7 +11,7 @@ my @querys = split(/&/, $request);
 my %query_hash;
 foreach my $query (@querys) {
 	my @key_value = split(/=/, $query);
-	$query_hash{$key_value[0]} = $key_value[1];
+	$query_hash{$key_value[0]} = !$key_value[1] ? "" : $key_value[1];
 }
 
 my @sort_values = sort(keys %query_hash);
